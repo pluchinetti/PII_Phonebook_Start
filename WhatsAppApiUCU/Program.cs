fa2f3b6ed6de86b3aa3e5fd6f71bc59bdfbf622d
pluchinetti <40801333+pluchinetti@users.noreply.github.com>
2018-10-03 17:55:43 -0300

    Added WhatsApp API folder

---
+++ WhatsAppApiUCU/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace WhatsAppApiUCU
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        { 
+            const string accountSid = "";
+            const string authToken = "";
+
+            var whatsApp = new WhatsAppApi(accountSid, authToken);
+            string sid = whatsApp.Send("+5989999999", "Hey there! I'm using WhatsAppUCU");
+            Console.WriteLine(sid);
+        }
+    }
+}
